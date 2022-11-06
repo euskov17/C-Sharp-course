950b303f1a7e6bf89b6175d7fcb853761c721244
Евгений Усков <st087349@student.spbu.ru>
2022-11-07 01:50:47 +0300

    add lab_9

---
+++ lab_9/Task4/Task4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+
+    public static String SearchFileInDirectory(String fName, String root)
+    {
+        //Console.WriteLine(root, fName);
+        try
+        {
+            var files = Directory.GetFiles(root);
+            foreach (var file in files)
+                if (file.Contains(fName))
+                    return file;
+            var dirs = Directory.GetDirectories(root);
+            foreach (var dir in dirs) {
+                var result = SearchFileInDirectory(fName, dir);
+                if (result != "Not Found")
+                    return result;
+            }
+        }
+        catch
+        {
+            return "Not Found";
+        }
+        return "Not Found";
+    }
+
+    public static String SearchFile(String fName)
+    {
+        return SearchFileInDirectory(fName, Directory.GetDirectoryRoot(Directory.GetCurrentDirectory()));
+    }
+
+
+    public static void Main()
+    {
+        Console.WriteLine(SearchFile("Task4.exe"));
+    }
+}
---
+++ lab_9/Task5/Task5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    public static string Rational(int a, int b)
+    {
+        List<int> remain = new List<int>();
+        List<int> deviders = new List<int>();
+        while (a != 0)
+        {
+            int dev = (10 * a) / b;
+            int rem = (10 * a) % b;
+            if (remain.Contains(rem))
+            {
+                var index = remain.IndexOf(rem);
+                if ((index == remain.Count - 1) & (dev != deviders[index])){
+                    deviders.Add(dev);
+                    index++;
+                }
+                String start = String.Concat(deviders.Take(index).Select(x => x.ToString()).ToList());
+                String finish = String.Concat(deviders.Skip(index).Select(x => x.ToString()).ToList());
+                return "0," + start + '(' + finish + ')';
+            }
+            else
+            {
+                deviders.Add(dev);
+                remain.Add(rem);
+            }
+            a = rem;
+        }
+        return "0," + String.Concat(deviders.Select(x => x.ToString()).ToList());
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Rational(2, 5));
+        Console.WriteLine(Rational(1, 6));
+        Console.WriteLine(Rational(1, 3));
+        Console.WriteLine(Rational(1, 7));
+        Console.WriteLine(Rational(1, 77));
+    }
+}
