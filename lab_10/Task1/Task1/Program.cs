c363e914804e97901370b6273df9828fbbe6ce8f
Евгений Усков <st087349@student.spbu.ru>
2022-11-12 18:34:07 +0300

    add lab10

---
+++ lab_10/Task1/Task1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using System.Windows.Markup;
+
+class Program
+{
+    public static int DefaultValue = 0;
+
+    public class BlackBox
+    {
+        private int innerValue;
+        private BlackBox(int innerValue)
+        {
+            this.innerValue = 0;
+        }
+        private BlackBox()
+        {
+            this.innerValue = DefaultValue;
+        }
+        private void Add(int addend)
+        {
+            this.innerValue += addend;
+        }
+        private void Subtract(int subtrahend)
+        {
+            this.innerValue -= subtrahend;
+        }
+        private void Multiply(int multiplier)
+        {
+            this.innerValue *= multiplier;
+        }
+        private void Divide(int divider)
+        {
+            this.innerValue /= divider;
+        }
+    }
+
+    public static void Main()
+    {
+   
+        Type type = typeof(BlackBox);
+        const BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+        Object bb = Activator.CreateInstance(typeof(BlackBox), true);
+        var innerValueField = type.GetField("innerValue", bindFlags);
+        String command = Console.ReadLine();
+        var args = command.Replace(')', '(').Split("(");
+        
+        while (type.GetMethod(args[0],bindFlags) is not null)
+        {
+            MethodInfo method = type.GetMethod(args[0], bindFlags);
+            int arg = int.Parse(args[1]);
+            method.Invoke(bb, new object[] { arg });
+            Console.WriteLine(innerValueField.GetValue(bb));
+            command = Console.ReadLine();
+            args = command.Replace(')', '(').Split("(");
+        }
+    }
+}
---
+++ lab_10/Task3/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+
+    public static int EnclosingEnvelopes(List<List<int>> envelops)
+    {
+        envelops.Sort((x, y) => (x[0].CompareTo(y[0]))); 
+        int[] dp = Enumerable.Repeat(1, envelops.Count).ToArray(); 
+        for (int i = 0; i < envelops.Count; ++i)
+            for (int j = i + 1; j < envelops.Count; ++j)
+                if (envelops[i][0] < envelops[j][0] & envelops[i][1] < envelops[j][1])
+                    dp[j] = Math.Max(dp[j], dp[i] + 1);
+        return dp.Max();
+    }
+
+
+    public static void Main()
+    {
+        var example1 = new List<List<int>>{ new List<int>{ 5, 4 }, new List<int>{ 6, 4 }, new List<int>{ 6, 7 },new List<int>{ 2, 3}};
+        var example2 = new List<List<int>> { new List<int>{ 1, 1 }, new List<int> { 1, 1 } };
+        Console.WriteLine(EnclosingEnvelopes(example1));
+        Console.WriteLine(EnclosingEnvelopes(example2));
+    }
+}
