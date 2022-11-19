be7a265cb01fe218d7773165c2065505203e15ab
Евгений Усков <st087349@student.spbu.ru>
2022-11-19 18:19:08 +0300

    add lab_11

---
+++ lab_11/Task1/Task1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    public class MyCash<T> where T : IDisposable
+    {
+        int capacity;        
+        int count;
+        int currentPriority;
+        Dictionary<T, int> data;
+
+        public MyCash(int capacity){
+            this.capacity = capacity;   
+            this.count = 0;
+            this.currentPriority = 0;
+            this.data = new Dictionary<T, int> ();
+        }
+        public void CallElement(T elem)
+        {
+            if (data.ContainsKey(elem))
+            {
+                data[elem] = currentPriority++;
+                return;
+            }
+            if (count == capacity)
+            {
+                var key = data.MinBy(x => x.Value).Key;
+                data.Remove(key);
+                key.Dispose();
+                GC.Collect();
+            }
+            else
+                count++;
+            data.Add(elem, currentPriority++);
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Call Dispose");
+            var keys = data.Keys.ToList();
+            data.Clear();
+            foreach (var key in keys)
+                key.Dispose();
+            count = 0;
+            GC.Collect();
+        }
+        
+        public List<T> getData()
+        {
+            return data.Keys.ToList();
+        }
+    }
+
+    public static void Main()
+    {
+        var cash = new MyCash<FileStream>(4);
+        var random = new Random();
+        var cur_indexes = new List<int>();
+        
+        // check that disposing elements is working
+        for (int i = 0; i < 20; i++)
+        {
+            int index = random.Next(10);
+            while (cur_indexes.Contains(index))
+                index = random.Next(10);
+            if (cur_indexes.Contains(index))
+                Console.WriteLine("We have an error");
+            cash.CallElement(File.Create(index.ToString() + ".txt", 1024));
+            Console.Write("index is " + index.ToString() + ":   ");
+            cur_indexes.Clear();
+            foreach (var d in cash.getData())
+            {
+                var s = d.Name.ToString();
+                var name = s.Substring(s.Length - 5, 1);
+                cur_indexes.Add(int.Parse(name));
+                Console.Write(name + ' ');
+            }
+            Console.WriteLine();
+        }
+
+        FileStream[] files = new FileStream[10];
+        cash.Dispose();
+        
+        for (int i = 0; i < 10; i++)
+        {
+            files[i] = File.Create(i.ToString() + ".txt", 1024);
+        }
+
+        // check that recalling is correct
+        for (int i = 0; i < 20; i++)
+        {
+            int index = random.Next(10);
+            Console.Write("index = " + index.ToString() + ":  ");
+            cash.CallElement(files[index]);
+            foreach (var d in cash.getData())
+            {
+                var s = d.Name.ToString();
+                var name = s.Substring(s.Length - 5, 1);
+                cur_indexes.Add(int.Parse(name));
+                Console.Write(name + ' ');
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ lab_11/Task2/Task2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    public static String Permutations(String s) {
+        var items = s.ToCharArray();
+        List<String> resultStrings = new List<String>();
+
+        int itemsNumber = items.Length;
+
+        var indexes = new int[itemsNumber];
+        for (int i = 0; i < itemsNumber; i++)
+            indexes[i] = 0;
+
+        resultStrings.Add(String.Join("", items));
+        
+        for (int i = 1; i < itemsNumber;)
+        {
+            if (indexes[i] < i)
+            {
+                if ((i & 1) == 1){
+                    var tmp = items[indexes[i]];
+                    items[indexes[i]] = items[i];
+                    items[i] = tmp;
+                }
+                else
+                {
+                    var tmp = items[i];
+                    items[i] = items[0];
+                    items[0] = tmp;
+                }
+
+                resultStrings.Add(String.Join("", items));
+                indexes[i]++;
+                i = 1;
+            }
+            else
+            {
+                indexes[i++] = 0;
+            }
+        }
+        var result = String.Join(" ", resultStrings.Distinct().ToList().OrderBy(x => x).ToList());
+        return result;
+    }
+
+	public static void Main()
+	{
+        Console.WriteLine(Permutations("AB"));
+        Console.WriteLine(Permutations("CD"));
+        Console.WriteLine(Permutations("EF"));
+        Console.WriteLine(Permutations("NOT"));
+        Console.WriteLine(Permutations("RAM"));
+        Console.WriteLine(Permutations("YAW"));
+    }
+}
