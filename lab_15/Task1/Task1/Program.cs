b1b9d3dd3aa63e41c113e4ed79ae3c686a112cd3
Evgeny  Uskov <80920825+euskov17@users.noreply.github.com>
2022-12-18 17:42:33 +0300

    add lab_15

---
+++ lab_15/Task1/Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static int N = 10;
+    static int X = 10;
+    static int steps = 10;
+    static int[] buffer = new int[X];
+    static Random random = new Random();
+    static int currentIter = 0;
+    static Mutex mutex = new Mutex();
+
+    static void bee_task()
+    {
+        while (steps > 0)
+        {
+            var time = random.Next(2000);
+            Thread.Sleep(time);
+            mutex.WaitOne();
+            if (currentIter < X)
+            {
+                buffer[currentIter++] = 1;
+            }
+            else
+            {
+                steps--;
+                currentIter = 0;
+                for (int i = 0; i < X; ++i) {
+                    buffer[i] = 0;
+                }
+            }
+            mutex.ReleaseMutex();
+        }
+    }
+
+    public static void Main()
+    {
+        List<Task> tasks = new List<Task>();
+        for (int i = 0; i < N; i++)
+        {
+            Task newTask = new Task(bee_task);
+            tasks.Add(newTask);
+            newTask.Start();
+        }
+        foreach (var task in tasks)
+            task.Wait();
+    }
+}
---
+++ lab_15/Task3/Task3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Channels;
+
+class Program
+{
+    static Mutex mutexObj = new Mutex();
+    public static int waiting = 0;
+
+    static int CHAIRS = 5;
+
+    static Semaphore haircutChair = new Semaphore(1, 1);
+    static Random random = new Random();
+
+    public static void Customer()
+    {
+        Thread thread = Thread.CurrentThread;
+        Console.WriteLine(thread.Name + " is here");
+        if (waiting <= CHAIRS)
+        {
+            Console.WriteLine(thread.Name +  " waits on place number " + waiting++.ToString());
+        }
+        else
+        {
+            Console.WriteLine(thread.Name + " left");
+            return;
+        }
+        haircutChair.WaitOne();
+
+        Console.WriteLine("\tHairdresser is working with " + thread.Name);
+        Thread.Sleep(random.Next(400));
+        waiting--;
+        Console.WriteLine("\tHairdresser finished with " + thread.Name);
+        haircutChair.Release();
+    }
+
+    static void Main()
+    {
+        int N = 15;
+        List<Thread> threads = new List<Thread>();
+
+        for (int i = 0; i < N; ++i)
+        {
+            Thread thr = new Thread(Customer);
+            thr.Name = "Customer " + i.ToString();
+            Thread.Sleep(random.Next(100));
+            threads.Add(thr);
+            thr.Start();
+        }
+        foreach (var thr in threads)
+            thr.Join();
+    }
+}
