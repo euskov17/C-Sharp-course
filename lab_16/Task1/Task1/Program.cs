b4a49bf5c8a770032e226eee281c2d1c17c5b0a7
Evgeny  Uskov <80920825+euskov17@users.noreply.github.com>
2022-12-25 02:34:07 +0300

    add lab16

---
+++ lab_16/Task1/Task1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+class Program
+{
+    public class ZeroEvenOdd
+    {
+        private int n;
+        private static CountdownEvent zeroWritten = new CountdownEvent(1);
+        private static CountdownEvent oddNow = new CountdownEvent(1);
+        private static CountdownEvent evenNow = new CountdownEvent(1);
+
+        public ZeroEvenOdd(int n)
+        {
+            this.n = n;
+        }
+
+        public void Zero(object printNumber)
+        {
+            Action<int> act = printNumber as Action<int>;
+            act(0);
+            zeroWritten.Signal();
+            oddNow.Signal();
+        }
+
+        public void Even(object printNumber)
+        {
+            zeroWritten.Wait();
+            Action<int> act = printNumber as Action<int>;
+            
+            int num = 1;
+            while (num < n)
+            {
+                evenNow.Wait();
+                act(2 * num++);
+                evenNow.Reset();
+                oddNow.Signal();
+            }
+        }
+
+        public void Odd(object printNumber)
+        {
+            zeroWritten.Wait();
+            Action<int> act = printNumber as Action<int>;
+            
+            int num = 0;
+            while (num < n)
+            {
+                oddNow.Wait();
+                act(1 + 2 * num++);
+                oddNow.Reset();
+                //oddNow.Signal();
+                evenNow.Signal();
+            }
+        }
+    }
+
+
+    public static void Main()
+    {
+        Action<int> printNumber = (x => Console.Write(x.ToString() + " "));
+        
+        int n = Convert.ToInt32(Console.ReadLine());
+        ZeroEvenOdd zeo = new ZeroEvenOdd(n);
+        
+        Thread a = new Thread(zeo.Zero);
+        Thread b = new Thread(zeo.Even);
+        Thread c = new Thread(zeo.Odd);
+        
+        c.Start(printNumber);
+        b.Start(printNumber);
+        a.Start(printNumber);
+
+        a.Join();
+        b.Join();
+        c.Join();
+    }
+}
---
+++ lab_16/Task4/Task4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+
+    public static int m = 5;
+    public static int n = 3;
+    public static int k = 7;
+    public static int p = 3;
+    public static int[,] A = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 }, { 13, 14, 15 } };
+    public static int[,] B = { { 1, 2, 3, 4, 5,6,7 }, { 8,9,10,11,12,13,14 }, { 15,16,17,18,19,20,21 }};
+    public static int[,] C = new int[m, k]; 
+    public static Mutex mutex = new Mutex();
+    public static int currentRow = 0, currentColumn = 0;
+    
+    public static void Multiply()
+    {
+        while (true)
+        {
+            Thread.Sleep(100);
+            mutex.WaitOne();
+            if (currentRow == m - 1 & currentColumn == k)
+            {
+                mutex.ReleaseMutex();
+                break;
+            }
+            int i = currentRow, j = currentColumn;
+            if (currentColumn == k - 1 & currentRow != m-1)
+            {
+                currentColumn = 0;
+                currentRow++;
+            }
+            else
+                currentColumn++;
+            mutex.ReleaseMutex();
+            for (int l = 0; l < n; l++)
+            {
+                C[i, j] += A[i, l] * B[l, j];
+            }
+        }
+    }
+
+    public static void Main()
+    {
+        List<Thread> threads = new List<Thread>();
+        for (int i = 0; i < p; i++)
+        {
+            Thread thr = new Thread(Multiply);
+            threads.Add(thr);
+            thr.Start();
+        }
+        foreach (Thread t in threads)
+            t.Join();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < k; j++)
+                Console.Write(C[i,j].ToString() + " ");
+            Console.WriteLine();
+        }
+    }
+}
